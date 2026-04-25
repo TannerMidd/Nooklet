1349dcf6809ba67b6ec79992dcaa7e7907145009
Tanner <tmiddleton@middmail.net>
2026-04-25 12:22:17 -0500

    Fix 0012 migration: SQLite cannot use non-constant defaults in ADD COLUMN

--- drizzle/0012_user_login_security.sql
+++ drizzle/0012_user_login_security.sql
@@ -2,4 +2,6 @@ ALTER TABLE `users` ADD `failed_login_attempts` integer DEFAULT 0 NOT NULL;
 --> statement-breakpoint
 ALTER TABLE `users` ADD `locked_until` integer;
 --> statement-breakpoint
-ALTER TABLE `users` ADD `password_changed_at` integer DEFAULT (unixepoch() * 1000) NOT NULL;
+ALTER TABLE `users` ADD `password_changed_at` integer DEFAULT 0 NOT NULL;
+--> statement-breakpoint
+UPDATE `users` SET `password_changed_at` = (unixepoch() * 1000) WHERE `password_changed_at` = 0;
